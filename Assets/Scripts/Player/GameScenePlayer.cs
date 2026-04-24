1e44f81c9aa1ad5d509fdd7e7d0bc8c77cf0104d
nemu415 <harukahmy@gmail.com>
2026-04-24 15:17:56 +0900

    Player

--- Assets/Scripts/Player/GameScenePlayer.cs
+++ Assets/Scripts/Player/GameScenePlayer.cs
@@ -8,20 +8,41 @@ public class GameScenePlayer : MonoBehaviour
     private float m_MoveSpeed = 15.0f;
 
     [SerializeField]
-    private float m_RotationSpeed = 10.0f;
+    private float m_RotationSpeed = 3.0f;
 
     [SerializeField]
     private float m_JumpPow = 5.0f;
     void Start()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        Vector3 moveXZ = Vector3.zero;
+
+        float mouseX = Input.GetAxis("Mouse X") * m_RotationSpeed;
+
+        transform.Rotate(0, mouseX, 0);
+
         if (Input.GetKeyDown(KeyCode.W))
         {
+            moveXZ = transform.forward * m_MoveSpeed;
+        }
 
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            moveXZ = -transform.forward * m_MoveSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            moveXZ += transform.right * m_MoveSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            moveXZ -= transform.right * m_MoveSpeed;
         }
     }
 }
