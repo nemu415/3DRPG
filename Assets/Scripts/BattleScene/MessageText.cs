cbcd495e43a7e5fe474c2eed5599647e06ba294a
soushi1116 <kurosawasoushi@gmail.com>
2026-04-28 14:17:28 +0900

    text追加

--- Assets/Scripts/BattleScene/MessageText.cs
+++ Assets/Scripts/BattleScene/MessageText.cs
@@ -1,16 +1,59 @@
+using NUnit.Framework;
+using TMPro;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VectorGraphics;
+using System.IO;
+using UnityEngine.UI;
 
 public class MessageText : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    int textNum = 0;
+    int count = 0;
+
+    [SerializeField]
+    private TextMeshProUGUI m_MessageText;
+
+    [SerializeField]
+    private TextAsset m_BattleText;
+
+    List<string[]> TextData = new List<string[]>();
+
+    private void Start()
     {
-        
+        StringReader reader = new StringReader(m_BattleText.text);
+
+        while (reader.Peek() != -1)
+        {
+            string line = reader.ReadLine();
+            TextData.Add(line.Split(','));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        string Times = TextData[textNum][count].ToString();
+
+        if (Times != "ENDTEXT")
+        {
+            if (Times != "END")
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    count++;
+                }
+
+                m_MessageText.text = Times;
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    count = 0;
+                    textNum++;
+                }
+            }
+        }
     }
 }
