4fde7c2f9b4f66708190893ad1d33932e4b4f829
Takananch <at8657403@gmail.com>
2026-07-21 15:12:04 +0900

    a

---
+++ Assets/Scripts/Random/RandomManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public static class RandomManager
+{
+    // min`max
+    public static int GetInt(int min, int max)
+    {
+        return Random.Range(min, max + 1);
+    }
+
+    // Z“”»’è
+    public static bool CheckPercent(int percent)
+    {
+        return Random.Range(0, 100) < percent;
+    }
+
+    // float”Å
+    public static float GetFloat(float min, float max)
+    {
+        return Random.Range(min, max);
+    }
+}
