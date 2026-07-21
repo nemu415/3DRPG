207729f3df5416a41173ef5c904488860a71765d
Takananch <at8657403@gmail.com>
2026-07-21 13:43:06 +0900

    a

--- Assets/Scripts/Random/RandomBase.cs
+++ Assets/Scripts/Random/RandomBase.cs
@@ -3,25 +3,25 @@ using UnityEngine;
 
 public class RandomBase:MonoBehaviour
 {
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            int count = Random.Range(1, 11); // 1～10回のランダムな試行回数を取得
-
-            int total = 0;  // 合計値を格納する変数
-
-            // countだけ1～100を取得
-            for (int i = 0; i < count; i++)
-            {
-                int randomValue=Random.Range(1, 1001);   // 1～1000の中からランダムな整数を取得
-
-                total += randomValue;   // 合計
-            }
-
-            // 結果を表示
-            Debug.Log("取得した回数："+count);
-            Debug.Log("合計値：" + total);
-        }
-    }
+    // private void Update()
+    // {
+    //     if(Input.GetKeyDown(KeyCode.Space))
+    //     {
+    //         int count = Random.Range(1, 11); // 1～10回のランダムな試行回数を取得
+    // 
+    //         int total = 0;  // 合計値を格納する変数
+    //
+    //         // countだけ1～100を取得
+    //         for (int i = 0; i < count; i++)
+    //         {
+    //             int randomValue=Random.Range(1, 1001);   // 1～1000の中からランダムな整数を取得
+    //
+    //             total += randomValue;   // 合計
+    //         }
+    //
+    //         // 結果を表示
+    //         Debug.Log("取得した回数："+count);
+    //         Debug.Log("合計値：" + total);
+    //     }
+    // }
 }
